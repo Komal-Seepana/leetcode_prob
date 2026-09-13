7b106c73049e7a61d7f134667cce7bda241b9427
Komal Rao Seepana <komalarao20070401@gmail.com>
2026-09-13 21:27:51 +0530

    Time: 9 ms (98.64%) | Memory: 50.4 MB (6.77%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int low = 0, high = 0;
+
+        for (int w : weights) {
+            low = Math.max(low, w);
+            high += w;
+        }
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            int count = 1;
+            int sum = 0;
+
+            for (int w : weights) {
+                if (sum + w > mid) {
+                    count++;
+                    sum = 0;
+                }
+                sum += w;
+            }
+
+            if (count <= days)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+
+        return low;
+    }
+}
